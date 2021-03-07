5d954a67f7d50bbd356dd0ae88246a71a7e2cabb
Ralf Lang <lang@b1-systems.de>
2021-03-07 18:20:27 +0100

    Actually act on component parameter

--- bin/entrypoint.sh
+++ bin/entrypoint.sh
@@ -3,11 +3,13 @@ echo "Config stage"
 ## Extract environment variables to determine UUT
 cp /srv/runtime/components/config/maintaina.conf.dist /srv/runtime/components/config/conf.php
 
-echo "Unit: $1"
-echo "Extra: $2"
+echo "Unit: $component"
+echo "Action: $action"
+echo "Branch: $branch"
 
-export UUT=view
-export BRANCH=FRAMEWORK_6_0
+export ACTION=${action:-qc}
+export UUT=${component:-view}
+export BRANCH=${branch:-FRAMEWORK_6_0}
 
 echo "Setup UUT stage"
 /srv/runtime/components/bin/horde-components git clone $UUT
